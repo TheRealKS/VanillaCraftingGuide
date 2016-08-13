9406221fa62dcfd66995c5ee6109c3f77ad3304a
koens <koen.sauren@gmail.com>
2016-08-13 10:40:05 +0200

    new stuff

--- src/main/java/com/koens/vanillacraftingguide/VanillaCraftingGuide.java
+++ src/main/java/com/koens/vanillacraftingguide/VanillaCraftingGuide.java
@@ -1,38 +1,11 @@
 package com.koens.vanillacraftingguide;
 
-import org.bukkit.Bukkit;
-import org.bukkit.Material;
-import org.bukkit.event.Listener;
-import org.bukkit.event.inventory.InventoryType;
-import org.bukkit.inventory.*;
 import org.bukkit.plugin.java.JavaPlugin;
-
-import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
 
 public final class VanillaCraftingGuide extends JavaPlugin {
 
-    private Map<Material, List<ItemStack>> shapedrecipes;
-    private Map<Material, List<ItemStack>> shapelessrecipes;
-
     @Override
     public void onEnable() {
-        Iterator<Recipe> allrecipes = getServer().recipeIterator();
-        while (allrecipes.hasNext()) {
-            Recipe recipe = allrecipes.next();
-            if (recipe != null && recipe.getResult() != null) {
-                if (recipe instanceof ShapedRecipe) {
-                    ShapedRecipe shapedRecipe = (ShapedRecipe) recipe;
-
-                    shapedrecipes.put(shapedRecipe.getResult(), shapedRecipe.)
-                } else if (recipe instanceof ShapelessRecipe) {
-                     ShapelessRecipe shapelessRecipe = (ShapelessRecipe) recipe;
-                } else {
-                    getLogger().warning("A recipe for item: '" + recipe.getResult().getType().toString() + "' is invalid!");
-                }
-            }
-        }
 
     }
 
